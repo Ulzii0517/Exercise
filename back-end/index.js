289387d67651fcbd9545a6bb433717fc429b183f
UlziiBadrakh <ulziibadrakh.bb@gmail.com>
2024-12-24 20:38:15 +0800

    first commit

---
+++ back-end/index.js
@@ -0,0 +1,20 @@
+
+// import express from "express"
+
+// const app = express();
+// const PORT = 3333;
+
+// app.listen(PORT, () => {
+//     console.log("server is working");
+    
+// })
+
+// import http from "http";
+// const server = http.createServer((request, response) => {
+//   response.end("Hello world");
+// });
+
+// server.listen(3333);
+
+
+
