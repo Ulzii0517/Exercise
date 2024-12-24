135fa6e37f69d081efb3367ea80265bd5a70bab8
UlziiBadrakh <ulziibadrakh.bb@gmail.com>
2024-12-24 22:20:21 +0800

    added crud

--- back-end/index.js
+++ back-end/index.js
@@ -1,13 +1,37 @@
+import express from "express";
+import bodyParser from "body-parser";
 
-// import express from "express"
+const app = express();
+app.use(bodyParser.json());
+const PORT = 3333;
 
-// const app = express();
-// const PORT = 3333;
+const users = [
+  { name: "John", age: 30 },
+  { name: "Paul", age: 28 },
+];
 
-// app.listen(PORT, () => {
-//     console.log("server is working");
+app.get("/users", (request, response) => {
     
-// })
+    
+  response.send(users);
+});
+
+app.post("/users", (request, response) => {
+    console.log(request.body);
+  response.send("Hello post huselt avlaa");
+});
+
+app.put("/users", (request, response) => {
+  response.send("Hello put huselt avlaa");
+});
+
+app.delete("/users", (request, response) => {
+  response.send("Hello delete huselt avlaa");
+});
+
+app.listen(PORT, () => {
+  console.log("server is working");
+});
 
 // import http from "http";
 // const server = http.createServer((request, response) => {
@@ -15,6 +39,3 @@
 // });
 
 // server.listen(3333);
-
-
-
