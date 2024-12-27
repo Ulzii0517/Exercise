ac5ed5e8dd467205636e84106168cc36e76da8d2
UlziiBadrakh <ulziibadrakh.bb@gmail.com>
2024-12-27 14:00:56 +0800

    deveployment on backend

--- back-end/index.js
+++ back-end/index.js
@@ -5,28 +5,90 @@ const app = express();
 app.use(bodyParser.json());
 const PORT = 3333;
 
-const users = [
-  { name: "John", age: 30 },
-  { name: "Paul", age: 28 },
+let users = [
+  { id: 1, name: "John", age: 30 },
+  { id: 2, name: "Paul", age: 28 },
+  { id: 3, name: "George", age: 27 },
+  { id: 4, name: "Ringo", age: 30 },
 ];
 
 app.get("/users", (request, response) => {
-    
-    
   response.send(users);
 });
 
 app.post("/users", (request, response) => {
-    console.log(request.body);
-  response.send("Hello post huselt avlaa");
+  const { name, age } = request.body;
+  const number = Math.random() * 100;
+  const result = Math.floor(number);
+
+  try {
+    if (!name || !age) {
+      return response.send("Ner esvel nas dutuu bn.");
+    } else {
+      if (typeof name !== "string") {
+        return response.send("Ner temdeg bish bn.");
+      }
+      if (typeof age !== "number") {
+        return response.send("Nas too bish bn.");
+      }
+      const newUser = {
+        id: result,
+        name: name,
+        age: age,
+      };
+
+      users.push(newUser);
+      response.send(users);
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
 app.put("/users", (request, response) => {
-  response.send("Hello put huselt avlaa");
+  const { id, updatedName, updatedAge } = request.body;
+  if (!id) {
+    return response.send("Id not found");
+  }
+  if (!updatedName && !updatedAge) {
+    return response.send("Name and age are both not found");
+  }
+
+  users.find((user) => {
+    if (user.id == id) {
+      if (updatedName) {
+        user.name = updatedName;
+      }
+      if (updatedAge) {
+        user.age = updatedAge;
+      }
+      return user;
+    }
+  });
+//   users.find((user) => {
+//     if (user.id == id) {
+//         user.name = updatedName ? updatedName : user.name;
+//         user.age = updatedAge ? updatedAge : user.age;
+//         return user;
+//     }
+//   })
 });
 
 app.delete("/users", (request, response) => {
-  response.send("Hello delete huselt avlaa");
+  const { id } = request.body;
+
+  const filteredUsers = users.filter((user) => {
+    if (user.id !== id) {
+      return user;
+    }
+  });
+
+  if (filteredUsers.length == users.length) {
+    return response.send("User is not exist");
+  }
+
+  users = filteredUsers;
+  response.send(users);
 });
 
 app.listen(PORT, () => {
